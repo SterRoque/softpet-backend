980dfba224146aaa4933ef731192ad5a2f862d92
Sterphane Roque <stermiss2@gmail.com>
2025-03-31 16:38:30 -0300

    feat: create pets resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AdminsModule } from './admins/admins.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [AdminsModule, AuthModule],
+  imports: [AdminsModule, AuthModule, PetsModule],
   controllers: [],
   providers: [
     {
