eecb6129f6a125b6c8b9df1c5c4fae8744a3aca5
Sterphane Roque <stermiss2@gmail.com>
2025-03-31 14:37:28 -0300

    feat: add admin business rules

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AdminsService } from './admins.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
@@ -12,23 +20,8 @@ export class AdminsController {
     return this.adminsService.create(createAdminDto);
   }
 
-  @Get()
-  findAll() {
-    return this.adminsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.adminsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
-    return this.adminsService.update(+id, updateAdminDto);
-  }
-
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.adminsService.remove(+id);
+    return this.adminsService.remove(id);
   }
 }
--- src/admins/admins.service.ts
+++ src/admins/admins.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AdminsService {
-  create(createAdminDto: CreateAdminDto) {
-    return 'This action adds a new admin';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createAdminDto: CreateAdminDto) {
+    const adminExists = await this.prisma.admin.findUnique({
+      where: {
+        email: createAdminDto.email,
+      },
+    });
+
+    if (adminExists) {
+      return new ConflictException({
+        message: 'admin already exists',
+      });
+    }
+
+    const admin = await this.prisma.admin.create({
+      data: {
+        first_name: createAdminDto.first_name,
+        last_name: createAdminDto.last_name,
+        email: createAdminDto.email,
+        password_hash: await bcrypt.hash(createAdminDto.password, 10),
+      },
+    });
+
+    return {
+      ...admin,
+      password_hash: undefined,
+    };
   }
 
-  findAll() {
-    return `This action returns all admins`;
-  }
+  async remove(id: string) {
+    const admin = await this.prisma.admin.findUnique({
+      where: {
+        id,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} admin`;
-  }
+    if (!admin) {
+      return new NotFoundException('admin not found');
+    }
 
-  update(id: number, updateAdminDto: UpdateAdminDto) {
-    return `This action updates a #${id} admin`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} admin`;
+    await this.prisma.admin.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
